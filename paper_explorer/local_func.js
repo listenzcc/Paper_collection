528a1917f7c464030bef604de64e56ed7ce2a849
zcc <listenzcc@mail.bnu.edu.cn>
2019-12-26 15:36:39 +0800

    Function add.
    Add paper_explorer function

---
+++ paper_explorer/local_func.js
@@ -0,0 +1,41 @@
+d3.json("papers.json").then(function(papers) {
+    d3.select("#left_bar")
+        .append("ol")
+        .selectAll("ol")
+        .data(papers)
+        .enter()
+        .append("li")
+        .text((d) => d.title)
+        .on("click", function(d) {
+            console.log(d.title)
+            console.log(d.doi)
+            console.log(d.rawpath)
+
+            // Display the paper
+            d3.select("#pdf_iframe")
+                .attr("src", d.rawpath)
+
+            // Append title session
+            d3.select("#paper_title")
+                .attr("placeholder", d.title)
+
+            // Append doi session
+            d3.select("#paper_doi")
+                .attr("placeholder", d.doi)
+
+            // Append rawpath session
+            d3.select("#paper_path")
+                .text(d.rawpath)
+        })
+})
+
+d3.select("#update_button")
+    .on("click", function() {
+        var title = document.getElementById("paper_title").textContent
+        if (title.length == 0) {
+            title = document.getElementById("paper_title").placeholder
+        }
+        console.log("Title is " + title)
+    })
+
+console.log('Done')
