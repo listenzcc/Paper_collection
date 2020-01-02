b3aca1fd1f0b7365a58d385732eed6451823c2f2
zcc <listenzcc@mail.bnu.edu.cn>
2020-01-02 17:15:11 +0800

    Function update.
    Improve python_httpserver for better parse html MISC format.
    
    Add get_pdf function for firefox.

--- paper_explorer/python_httpserver.py
+++ paper_explorer/python_httpserver.py
@@ -17,30 +17,49 @@ custom_df = pd.read_json(os.path.join('..', 'paper_jsons', 'custom.json'))
 
 class DataWorker():
     def __init__(self):
+        # Init path and df
+        # pdfpath: path of pdf files
+        # custom_df: custom df
+        # raw_df: raw df
+        self.pdfpath = 'C:\\Users\\liste\\OneDrive\\Documents\\schorlar\\buffer'
         self.raw_df = pd.read_json(os.path.join('..', 'paper_jsons', 'raw.json'))
         self.custom_df = pd.read_json(os.path.join('..', 'paper_jsons', 'custom.json'))
 
     def update_custom(self, query):
+        # Update custom df as query
         uid = query.get('uid', 'uid')
-        rawpath = query.get('rawpath', 'rawpath')
-        rawpath = rawpath.replace('%20', ' ').replace('\\\\', '\\')
-
-        if not hashlib.new('md5', rawpath.encode()).hexdigest() == uid:
-            print('Error ' * 10)
-            print(uid)
-            print(hashlib.new('md5', rawpath.encode()).hexdigest())
-            raise Exception('Hash md5 not match.')
-
+        # New Series
         se = pd.Series(name=uid)
-
+        # Inject items as query
         for key in query:
             if key in ['uid', 'rawpath', 'set']:
                 continue
             se[key] = query[key]
+        # Override uid row of custom_df
+        self.custom_df.drop(index=uid, inplace=True, errors='ignore')
         self.custom_df = self.custom_df.append(se)
+        # Replace NaN into '--'
+        self.custom_df[self.custom_df.isna()] = ''
+        print(self.custom_df)
+    
+    def get_pdf(self, fname):
+        # Get pdf from pdfpath
+        # fname: fname of pdf file
+        # Replace %20 into [space]
+        fname = fname
+        fullpath = os.path.join(self.pdfpath, fname)
 
-        self.custom_df[self.custom_df.isna()] = '--'
+        # Check if file exists
+        # Return if not
+        if not os.path.exists(fullpath):
+            raise FileNotFoundError(fullpath)
 
+        # Read pdf file and return as bytes
+        with open(fullpath, 'rb') as fp:
+            pdf_bits_list = fp.readlines()
+        return b''.join(pdf_bits_list)
+
+# Init worker
 worker = DataWorker()
 
 class Resquest(BaseHTTPRequestHandler):
@@ -51,31 +70,45 @@ class Resquest(BaseHTTPRequestHandler):
         # Claim return content-type as json
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
         self.urlparse()
+
         # Get raw_df
         if self.query.get('get', '') == 'raw':
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
             self.wfile.write(worker.raw_df.to_json(orient='records').encode())
             return
+
         # Get custom_df
         if self.query.get('get', '') == 'custom':
-            self.wfile.write(worker.custom_df.to_json(orient='records').encode())
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(worker.custom_df.to_json().encode())
             return
+
+        # Get PDF file
+        if self.query.get('get', '') == 'pdf':
+            self.send_header('Content-type', 'application/pdf')
+            self.end_headers()
+            self.wfile.write(worker.get_pdf(self.query.get('fname', '')))
+            return
+
         # Update custom_df
         if self.query.get('set', '') == 'custom':
-            try:
-                worker.update_custom(self.query)
-            except Exception as e:
-                self.error_message(repr(e))
-            pprint(worker.custom_df.to_string())
-            self.wfile.write(worker.custom_df.to_json(orient='records').encode())
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            worker.update_custom(self.query)
+            self.wfile.write(worker.custom_df.to_json().encode())
             return
+
         # Hello there
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
         self.wfile.write(json.dumps([
             'Hello there.',
             '?get=raw: get raw_df.json',
             '?get=custom: get custom_df.json',
+            '?get=pdf, fname=fname: get pdf file of filename',
             '?set=custom, uid=[], rawpath=[]: set custom_df.json as uid and rawpath'
         ]).encode())
 
@@ -107,7 +140,7 @@ class Resquest(BaseHTTPRequestHandler):
                 continue
             # Setup query
             a, b = query.split('=', 1)
-            self.query[a] = b
+            self.query[a] = b.replace('%20', ' ')
         # Log requests
         self.log_messages([parsed, self.query])
 
