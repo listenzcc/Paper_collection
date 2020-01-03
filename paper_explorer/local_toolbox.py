af5783f3fd4283850f846319ed1a6e2f7a5ea6a6
zcc <listenzcc@mail.bnu.edu.cn>
2020-01-03 16:39:36 +0800

    Function update.
    Immigrate RequestHandler into local_toolbox.
    Improve DataWorker and RequestHandler on useablity.

--- paper_explorer/local_toolbox.py
+++ paper_explorer/local_toolbox.py
@@ -1,11 +1,14 @@
 import os
 import json
+import time
+import urllib.parse
 import pandas as pd
+from pprint import pprint
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
 class DataWorker():
-    def __init__(self, paths):
+    def __init__(self, paths, newcustom=False):
         # Init path and df
         # pdfdir: dir path of pdf files
         # custom_df: custom df
@@ -13,7 +16,10 @@ class DataWorker():
         self.paths = paths
         self.pdfdir = paths.get('pdfdir')
         self.raw_df = pd.read_json(paths.get('raw_df'))
-        self.custom_df = pd.read_json(paths.get('custom_df'))
+        if newcustom:
+            self.custom_df = pd.DataFrame()
+        else:
+            self.custom_df = pd.read_json(paths.get('custom_df'))
 
     def update_custom(self, query):
         # Update custom df as query
@@ -28,9 +34,14 @@ class DataWorker():
         # Override uid row of custom_df
         self.custom_df.drop(index=uid, inplace=True, errors='ignore')
         self.custom_df = self.custom_df.append(se)
-        # Replace NaN into '--'
+        # Replace NaN into ''
         self.custom_df[self.custom_df.isna()] = ''
         print(self.custom_df)
+
+    def save_custom(self):
+        print(self.custom_df)
+        print('Saving custom as {}'.format(self.paths.get('custom_df')))
+        self.custom_df.to_json(self.paths.get('custom_df'))
 
     def get_pdf(self, fname):
         # Get pdf from pdfdir
@@ -48,3 +59,89 @@ class DataWorker():
         with open(fullpath, 'rb') as fp:
             pdf_bits_list = fp.readlines()
         return b''.join(pdf_bits_list)
+
+
+class ResquestHandler(BaseHTTPRequestHandler):
+    def send_response_content(self, content_type, content, allow_origin=True):
+        if allow_origin:
+            self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-Type', 'application/{}'.format(content_type))
+        self.end_headers()
+        self.wfile.write(content)
+
+    def do_GET(self):
+        # Overwrite do_GET method of BaseHTTPRequestHandler
+        # Parse request first
+        self.urlparse()
+        # Link data worker
+        worker = self.server.worker
+
+        # Send 200 response
+        self.send_response(200)
+
+        # Get raw_df
+        if self.query.get('get', '') == 'raw':
+            self.send_response_content(
+                'json', worker.raw_df.to_json(orient='records').encode())
+            return
+
+        # Get custom_df
+        if self.query.get('get', '') == 'custom':
+            self.send_response_content(
+                'json', worker.custom_df.to_json().encode())
+            return
+
+        # Get PDF file
+        if self.query.get('get', '') == 'pdf':
+            self.send_response_content(
+                'pdf', worker.get_pdf(self.query.get('fname', '')))
+            return
+
+        # Update custom_df
+        if self.query.get('set', '') == 'custom':
+            worker.update_custom(self.query)
+            self.send_response_content(
+                'json', worker.custom_df.to_json().encode())
+            return
+
+        # Hello there
+        self.send_response_content('json', json.dumps([
+            'Hello there.',
+            '?get=raw: get raw_df.json',
+            '?get=custom: get custom_df.json',
+            '?get=pdf&fname=fname: get pdf file of filename',
+            '?set=custom&uid=[xx]&a=b&c=d: set custom_df.json as uid with a=b, c=d'
+        ]).encode())
+
+    def error_message(self, error_message):
+        message = dict(
+            url=self.path,
+            query=self.query,
+            error=error_message
+        )
+        self.log_messages(message, end='!!!!!!!Error Error Error!!!!!!')
+        return message
+
+    def log_messages(self, messages='[None]', end=''):
+        # Logger
+        print('-' * 80)
+        print('[{}]'.format(time.time()))
+        pprint(messages)
+        print(end)
+
+    def urlparse(self):
+        # Custom urlparse
+        parsed = urllib.parse.urlparse(self.path)
+        # Format self.query
+        self.query = dict()
+        # Parse query in requests
+        for query in parsed.query.split('&'):
+            # Ignore inlegal query
+            if '=' not in query:
+                continue
+            # Setup query
+            a, b = query.split('=', 1)
+            # Fix known convertion issue
+            self.query[a] = b.replace('%20', ' ')
+        # Log requests
+        self.log_messages([parsed, self.query])
--- paper_explorer/python_httpserver.py
+++ paper_explorer/python_httpserver.py
@@ -1,11 +1,9 @@
 import os
-import time
-import json
 import webbrowser
-import urllib.parse
-from pprint import pprint
 from local_toolbox import DataWorker
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from local_toolbox import ResquestHandler
+from http.server import HTTPServer
+
 
 # Init host
 domain = 'localhost'
@@ -19,101 +17,29 @@ paths = dict(
     raw_df=os.path.join('..', 'paper_jsons', 'raw.json'),
     custom_df=os.path.join('..', 'paper_jsons', 'custom.json')
 )
-worker = DataWorker(paths)
+worker = DataWorker(paths, newcustom=True)
 
 
-class Resquest(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Overwrite do_GET method of BaseHTTPRequestHandler
-        # Send 200 response
-        # Claim return content-type as json
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.urlparse()
+class http_server():
+    def __init__(self, worker, host, request_handler_class):
+        self.server = HTTPServer(host, request_handler_class)
+        self.server.worker = worker
+        print('Starting server, listen at: {}:{}'.format(*host))
 
-        # Get raw_df
-        if self.query.get('get', '') == 'raw':
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            self.wfile.write(worker.raw_df.to_json(orient='records').encode())
-            return
-
-        # Get custom_df
-        if self.query.get('get', '') == 'custom':
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            self.wfile.write(worker.custom_df.to_json().encode())
-            return
-
-        # Get PDF file
-        if self.query.get('get', '') == 'pdf':
-            self.send_header('Content-type', 'application/pdf')
-            self.end_headers()
-            self.wfile.write(worker.get_pdf(self.query.get('fname', '')))
-            return
-
-        # Update custom_df
-        if self.query.get('set', '') == 'custom':
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            worker.update_custom(self.query)
-            self.wfile.write(worker.custom_df.to_json().encode())
-            return
-
-        # Hello there
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps([
-            'Hello there.',
-            '?get=raw: get raw_df.json',
-            '?get=custom: get custom_df.json',
-            '?get=pdf, fname=fname: get pdf file of filename',
-            '?set=custom, uid=[], rawpath=[]: set custom_df.json as uid and rawpath'
-        ]).encode())
-
-    def error_message(self, error_message):
-        message = dict(
-            url=self.path,
-            query=self.query,
-            error=error_message
-        )
-        self.log_messages(message)
-        return message
-
-    def log_messages(self, messages='[None]', end=''):
-        # Logger
-        print('-' * 80)
-        print('[{}]'.format(time.time()))
-        pprint(messages)
-        print(end)
-
-    def urlparse(self):
-        # Custom urlparse
-        parsed = urllib.parse.urlparse(self.path)
-        # Format self.query
-        self.query = dict()
-        # Parse query in requests
-        for query in parsed.query.split('&'):
-            # Ignore inlegal query
-            if '=' not in query:
-                continue
-            # Setup query
-            a, b = query.split('=', 1)
-            self.query[a] = b.replace('%20', ' ')
-        # Log requests
-        self.log_messages([parsed, self.query])
+    def start(self):
+        try:
+            self.server.serve_forever()
+        except KeyboardInterrupt:
+            print('Interrupted')
+        finally:
+            self.server.worker.save_custom()
+            self.server.server_close()
 
 
-server = HTTPServer(host, Resquest)
-print('Starting server, listen at: {domain}:{port}'.format(
-    domain=domain, port=port))
+server = http_server(worker, host, ResquestHandler)
+
 
 if __name__ == '__main__':
     url = 'http://{domain}:{port}'.format(domain=domain, port=port)
     webbrowser.open(url)
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print('keyboard interrupt')
-    finally:
-        server.server_close()
+    server.start()
